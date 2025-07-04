e0b2df163cf48b09a4ac1ff84df304301eab38c8
B Ramanarayanan <b.ramanarayanan@ibm.com>
2025-07-04 19:09:55 +0530

    API invoked

---
+++ RegForm/API/CustomerController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RegForm.Data;
+
+namespace RegForm.API
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CustomerController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+
+        public string test()
+        {
+            return "API is working fine!"; // Test API endpoint
+        }
+
+        [HttpGet]
+        public IActionResult GetUsers() // To get the list of users
+        {
+            var users = _context.Users.ToList();
+            return Ok(users);
+        }
+    }
+}
--- RegForm/Program.cs
+++ RegForm/Program.cs
@@ -26,6 +26,7 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapStaticAssets();
+app.MapControllers();
 
 app.MapControllerRoute(
     name: "default",
