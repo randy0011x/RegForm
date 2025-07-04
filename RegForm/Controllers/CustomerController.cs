af57269e44eb8bcaf846045b5bc24fc1200cfa8c
BRamanarayanan <BRamanarayanan@IBM-4FJR434>
2025-07-04 16:07:42 +0530

    Add project files.

---
+++ RegForm/Controllers/CustomerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RegForm.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ RegForm/Data/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RegForm.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+    }
+}
