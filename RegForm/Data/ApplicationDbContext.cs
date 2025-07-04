891d345757ad6915084827221e66141378f95488
B Ramanarayanan <b.ramanarayanan@ibm.com>
2025-07-04 16:36:20 +0530

    Designed Database for Customers in dbo.Users and added necessaries in program.cs and application.json

--- RegForm/Data/ApplicationDbContext.cs
+++ RegForm/Data/ApplicationDbContext.cs
@@ -1,8 +1,18 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using RegForm.Models;
 
 namespace RegForm.Data
 {
-    public class ApplicationDbContext :DbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<UserModel> Users { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<UserModel>();
+        }
     }
 }
